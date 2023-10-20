8f4631399684b860e950af359a3cf3423982d09b
Evan BOISSONNOT <evan@boissonnot.fr>
2023-10-20 11:22:57 +0200

    feat: avec bootstrap

--- react/vite-project/src/features/games/ListGameFinal/index.tsx
+++ react/vite-project/src/features/games/ListGameFinal/index.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { Games } from "../models/game";
 import { GameRowFinal } from "./GameRowFinal";
+import { Col, Row } from "react-bootstrap";
 
-export type ListGameProps = { items: Games; }
+export type ListGameProps = { items: Games };
 
 // Head component : il sait les data => le state
 export const ListGameFinal = (props: ListGameProps) => {
-  console.info('ListGameFinal');
+  console.info("ListGameFinal");
 
   //let title = 'Liste des games';
-  const [title, setTitle] = useState('Liste des games');
+  const [title, setTitle] = useState("Liste des games");
   // State
   // const games = getGamesBusinessWithSuccess(getGamesInfraTableauEnMemoire);
 
@@ -24,19 +25,26 @@ export const ListGameFinal = (props: ListGameProps) => {
 
   return (
     <>
-      <h1>{title}</h1>
+      <Row>
+        <Col>
+          <h1>{title}</h1>
+        </Col>
+        <Col>
+          <button className="success" onClick={changeTitle}>AddDate</button>
+        </Col>
+      </Row>
 
-      <button onClick={changeTitle}>AddDate</button>
-      
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>{elementsTsx}</tbody>
-      </table>
-      {/* <GameRowFinal id="15494" type="Quidditch" dateDebut="26/09/98" /> */}
+      <Row>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>{elementsTsx}</tbody>
+        </table>
+        {/* <GameRowFinal id="15494" type="Quidditch" dateDebut="26/09/98" /> */}
+      </Row>
     </>
   );
 };
